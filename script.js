714161a4a05778b2ebdc39dcab213d0c78d57c62
Karan <pillaikaran9@gmail.com>
2023-02-03 22:48:44 +0530

    ADD rainbow color button

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 let gridValue = prompt("Choose a gird Value between 16 and 64");
 const box = document.querySelector(".box");
+//RGB Values for rainbow
 
+
+//function to create a grid
 function createGrid(gridValue){
     box.setAttribute(`style`, `display: grid;grid-template-columns: repeat(${gridValue},1fr); grid-template-rows: repeat(${gridValue}, 1fr);`);
     for (let i = 0; i < gridValue*gridValue; i++){
@@ -14,6 +17,7 @@ function createGrid(gridValue){
 }
 createGrid(gridValue);
 
+//functon to delete grids before creating a new one.
 function deleteGridElements(){
     const allDiv = document.querySelectorAll(".boxStyle");
     allDiv.forEach((allDiv)=>{
@@ -21,6 +25,7 @@ function deleteGridElements(){
     })
 }
 
+//Creates a new grid
 const newGrid = document.querySelector(".newGrid");
 newGrid.addEventListener("click", ()=>{
     gridValue = prompt("Choose a gird Value between 16 and 64");
@@ -28,8 +33,41 @@ newGrid.addEventListener("click", ()=>{
     createGrid(gridValue);
 })
 
+//choses black color
+const black = document.querySelector(".black");
+black.addEventListener('click', ()=>{
+    const divs = document.querySelectorAll(".boxStyle");
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', ()=>{
+            div.style.backgroundColor = 'black';
+        })
+    })
+})
+
+
+function getRandomColor(){
+    const red = Math.floor(Math.random()*255);
+    const blue = Math.floor(Math.random()*255);
+    const green = Math.floor(Math.random()*255);
+
+    const color = `rgb(${red},${blue},${green})`;
+    return color;
+}
+
+//applies random color
+const rainbow = document.querySelector(".rainbow");
+rainbow.addEventListener('click', () => { 
+    const divs = document.querySelectorAll(".boxStyle");
+    divs.forEach((div) => {
+        div.addEventListener('mouseover', () => {
+            div.style.backgroundColor = getRandomColor();
+        })
+    })
+})
+
+//resets the color
 const reset = document.querySelector('.reset');
-reset.addEventListener('click', ()=>{
+reset.addEventListener('click', () => {
     const divs = document.querySelectorAll('.boxStyle');
     divs.forEach((div)=>{
         div.style.backgroundColor = 'white';
